2c625c5c47efc28c4c1355328bade7bb3ce99bce
a20davidmg <davidmaringomez81@gmail.com>
2023-04-12 21:49:58 +0200

    Ejercicio 5 sin acabar

--- Psp3Evaluacion/src/Ej05/Cliente05.java
+++ Psp3Evaluacion/src/Ej05/Cliente05.java
@@ -1,7 +1,28 @@
 package Ej05;
 
-public class Cliente05 {
-    public static void main(String[] args) {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class Cliente05 {
+    public static void main(String[] args) throws Exception {
+        InetAddress destino = InetAddress.getLocalHost();
+        int port = 12345;
+        byte[] mensaje = new byte[1024];
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introduce una cadena:");
+        String cadena = scan.nextLine();
+        System.out.println("Mensaje: " + cadena);
+        mensaje = cadena.getBytes();
+
+        //Se construye el Datagram para enviar
+        DatagramPacket envio = new DatagramPacket(mensaje, mensaje.length, destino, port);
+        DatagramSocket socket = new DatagramSocket(34567);
+
+        socket.send(envio);
+        socket.close();
     }
 }
--- Psp3Evaluacion/src/Ej05/Servidor05.java
+++ Psp3Evaluacion/src/Ej05/Servidor05.java
@@ -1,7 +1,26 @@
 package Ej05;
 
-public class Servidor05 {
-    public static void main(String[] args) {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class Servidor05 {
+    public static void main(String[] args) throws Exception {
+        byte[] buffer = new byte[1024];
+        DatagramSocket socket = new DatagramSocket(12345);
+
+        System.out.println("Esperando el mensaje...");
+        DatagramPacket recibo = new DatagramPacket(buffer, buffer.length);
+        socket.receive(recibo);
+
+        int bytesRec = recibo.getLength();
+        String paquete = new String(recibo.getData());
+
+        System.out.println("Numero de bytes recibidos: " + bytesRec);
+        System.out.println("Mensaje recibido: " + paquete.trim());
+
+        socket.close();
     }
 }
